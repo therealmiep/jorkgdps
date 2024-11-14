a2471d1bddb0d256d670d1924bfd61b367ca5b93
Maxe <sebasits14@gmail.com>
2024-11-14 16:22:24 -0500

    Updated users.php

--- demonlist/users.php
+++ demonlist/users.php
@@ -1,6 +1,6 @@
 <?php
 $users = [
-    'user1' => password_hash('password1', PASSWORD_DEFAULT),
-    'user2' => password_hash('password2', PASSWORD_DEFAULT),
-];
+    '' => password_hash('', PASSWORD_DEFAULT),
+    '' => password_hash('', PASSWORD_DEFAULT),
+]; // 'user' => password_hash:('password', PASSWORD_DEFAULT),
 ?>
